4779d9e479c2e445f2899c28a8b4d124c972760a
Darric <darric.lu@gmail.com>
2020-08-28 23:25:40 +0800

    Project init

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const port = 3000
+const bullshitMaker = require('./bullshit_maker')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`Express app listening on port ${port}.`)
+})
